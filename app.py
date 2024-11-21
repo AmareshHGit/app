bb2d7df5314013041aeace5bdee2dbc85f1c9853
AmareshHGit <amaresh.georgian@gmail.com>
2024-11-21 03:36:07 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,35 +1,32 @@
-
 from flask import Flask
 from flask import request, jsonify
-
-
 
 # A very simple Flask Hello World application for you to get started with
 
 app = Flask(__name__)
-#app = Flask(flask_skeleton)
 
 @app.route('/')
 def home():
     student_number = "200625943" 
     return jsonify({"student_number": student_number})
 
-@app.route('/webhook', methods = ['GET', 'POST'])
+@app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-        req = request.get_json(silent = True, force = True)
-        fulfillmentText = ''
+    req = request.get_json(silent=True, force=True)
+    fulfillmentText = ''
 
-        query_result = req.get('queryResult')
-        parameters = queryResults.get('parameters', '')
-        if query_result.get('action') == 'get.name':           
-        fulfillmentText = "Hello "+str(parameters.get('name', ''))+", glad to meet you!!"
-        return {
-          "fulfillmentText": fulfillmentText,
-          "source": "webhookdata"
-          }
-        
+    query_result = req.get('queryResult')
+    parameters = query_result.get('parameters', '')  # Corrected the variable name here
+
+    if query_result.get('action') == 'get.name':
+        fulfillmentText = "Hello " + str(parameters.get('name', '')) + ", glad to meet you!!"
+    
+    return {
+        "fulfillmentText": fulfillmentText,
+        "source": "webhookdata"
+    }
 
 if __name__ == '__main__':
- 
-   app.run(debug=True)
+    app.run(debug=True)
+
 
